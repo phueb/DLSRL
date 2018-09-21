10aea6064cbb981562bbd0ec84968b3d625caf8c
phueb <info@philhuebner.com>
2018-09-21 14:06:06 -0500

    remove gpu_temp check

--- check_gpu_temp.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import re
-
-
-lines = subprocess.getoutput('nvidia-smi -q').split()
-for n, line in enumerate(lines):
-    match=re.search('Temp', line)
-    if match:
-        print(lines[n:n+5])
