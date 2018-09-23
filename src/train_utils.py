39c6c0ee7282093c27270ef922f0857ecbf1346a
phueb <phueb001@ucr.edu>
2018-09-22 22:39:18 -0500

    added note about f1-calculation mistake (BIO labeling must be considered)

--- src/train_utils.py
+++ src/train_utils.py
@@ -84,7 +84,7 @@ def print_f1(epoch, method, f1):
     print('epoch {:>3} method={} | f1={:.2f}'.format(epoch, method, f1))
 
 
-def f1_conll05(gold, pred, lengths, exclude_label_one=True):  # TODO test
+def f1_conll05(gold, pred, lengths, exclude_label_one=True):  # TODO due to BIO tagging complete arguments are not compared (only B-to-B and I-to_I)
     hits = 0
     over_predictions = 0
     misses = 0
