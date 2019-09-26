12cc002554546b0b9e648eca49aeec2546c60ba8
phueb <info@philhuebner.com>
2019-09-26 12:27:31 -0500

    working on implementing f1 evaluation for allen nlp toolkit srl model

--- dlsrl/job.py
+++ dlsrl/job.py
@@ -4,7 +4,6 @@ from sklearn.metrics import f1_score
 import numpy as np
 import pandas as pd
 import torch
-from itertools import chain
 
 from allennlp.data.vocabulary import Vocabulary
 from allennlp.common.params import Params as AllenParams
@@ -19,9 +18,6 @@ from dlsrl.utils import get_batches, shuffle_stack_pad, count_zeros_from_end
 from dlsrl.eval import print_f1, f1_official_conll05
 from dlsrl.model import AllenSRLModel
 from dlsrl import config
-
-
-EVALUATE_ON_DEV = False  # TODO testing
 
 
 class Params:
@@ -103,7 +99,7 @@ def main(param2val):
                           encoder=encoder,
                           initializer=initializer,
                           binary_feature_dim=100)
-    model.cuda()  # TODO test
+    model.cuda()
 
     from allennlp.common.checks import check_for_gpu
     check_for_gpu(device_id=0)
@@ -124,85 +120,87 @@ def main(param2val):
         print('Epoch: {}'.format(epoch))
         print('===========')
 
-        # prepare data for epoch
-        train_x1, train_x2, train_y = shuffle_stack_pad(data.train,
-                                                        batch_size=params.batch_size)  # returns int32
-        dev_x1, dev_x2, dev_y = shuffle_stack_pad(data.dev,
-                                                  batch_size=config.Eval.dev_batch_size,
-                                                  shuffle=False)
-
         # ----------------------------------------------- start evaluation
 
-        if EVALUATE_ON_DEV:
 
-            # per-label f1 evaluation
-            all_gold_label_ids_no_pad = []
-            all_pred_label_ids_no_pad = []
+        # per-label f1 evaluation
+        all_gold_label_ids_no_pad = []
+        all_pred_label_ids_no_pad = []
 
-            # conll05 evaluation data
-            all_sentence_pred_labels_no_pad = []
-            all_sentence_gold_labels_no_pad = []
-            all_verb_indices = []
-            all_sentences_no_pad = []
+        # conll05 evaluation data
+        all_sentence_pred_labels_no_pad = []
+        all_sentence_gold_labels_no_pad = []
+        all_verb_indices = []
+        all_sentences_no_pad = []
 
-            model.eval()
-            for step, (x1_b, x2_b, y_b) in enumerate(get_batches(dev_x1, dev_x2, dev_y, config.Eval.dev_batch_size)):
+        model.eval()
+        dev_generator = bucket_batcher(data.dev_instances, num_epochs=1)
+        for step, batch in enumerate(dev_generator):
 
-                # get predicted label_ids from model
+            # to cuda
+            batch['tokens']['tokens'] = batch['tokens']['tokens'].cuda()
+            batch['verb_indicator'] = batch['verb_indicator'].cuda()
+            batch['tags'] = batch['tags'].cuda()
 
-                softmax_2d = model()# TODO
-                softmax_3d = np.reshape(softmax_2d, (*np.shape(x1_b), data.num_labels))  # 1st dim is batch_size
-                batch_pred_label_ids = np.argmax(softmax_3d, axis=2)  # [batch_size, seq_length]
-                batch_gold_label_ids = y_b  # [batch_size, seq_length]
-                assert np.shape(batch_pred_label_ids) == (config.Eval.dev_batch_size, np.shape(x1_b)[1])
+            # get predictions
+            output_dict = model(**batch)  # input is dict[str, torch tensor]
+            softmax_3d = output_dict['class_probabilities'].detach().cpu().numpy()  # 1st dim is batch_size
 
-                # collect data for evaluation
-                for x1_row, x2_row, gold_label_ids, pred_label_ids, in zip(x1_b,
-                                                                           x2_b,
-                                                                           batch_gold_label_ids,
-                                                                           batch_pred_label_ids):
+            # TODO test softmax_3d
+            print(softmax_3d.shape)
+            print(softmax_3d.sum(axis=2))
 
-                    sentence_length = len(x1_row) - count_zeros_from_end(x1_row)
-
-                    assert count_zeros_from_end(x1_row) == count_zeros_from_end(gold_label_ids)
-                    sentence_gold_labels = [data.sorted_labels[i] for i in gold_label_ids]
-                    sentence_pred_labels = [data.sorted_labels[i] for i in pred_label_ids]
-                    verb_index = np.argmax(x2_row)
-                    sentence = [data.sorted_words[i] for i in x1_row]
-
-                    # collect data for conll-05 evaluation + remove padding
-                    all_sentence_pred_labels_no_pad.append(sentence_pred_labels[:sentence_length])
-                    all_sentence_gold_labels_no_pad.append(sentence_gold_labels[:sentence_length])
-                    all_verb_indices.append(verb_index)
-                    all_sentences_no_pad.append(sentence[:sentence_length])
-
-                    # collect data for per-label evaluation
-                    all_gold_label_ids_no_pad += list(gold_label_ids[:sentence_length])
-                    all_pred_label_ids_no_pad += list(pred_label_ids[:sentence_length])
-
-            print('Number of sentences to evaluate: {}'.format(len(all_sentences_no_pad)))
-
-            for label in all_sentence_gold_labels_no_pad:
-                assert label != config.Data.pad_label
-
-            # evaluate f1 score computed over single labels (not spans)
-            # f1_score expects 1D label ids (e.g. gold=[0, 2, 1, 0], pred=[0, 1, 1, 0])
-            print_f1(epoch, 'weight', f1_score(all_gold_label_ids_no_pad, all_pred_label_ids_no_pad, average='weighted'))
-            print_f1(epoch, 'macro ', f1_score(all_gold_label_ids_no_pad, all_pred_label_ids_no_pad, average='macro'))
-            print_f1(epoch, 'micro ', f1_score(all_gold_label_ids_no_pad, all_pred_label_ids_no_pad, average='micro'))
-
-            # evaluate with official conll05 perl script with Python interface provided by Allen AI NLP toolkit
-            sys.stdout.flush()
-            print('=============================================')
-            print('Official Conll-05 Evaluation on Dev Split')
-            dev_f1 = f1_official_conll05(all_sentence_pred_labels_no_pad,  # List[List[str]]
-                                         all_sentence_gold_labels_no_pad,  # List[List[str]]
-                                         all_verb_indices,  # List[Optional[int]]
-                                         all_sentences_no_pad)  # List[List[str]]
-            print_f1(epoch, 'conll-05', dev_f1)
-            dev_f1s.append(dev_f1)
-            print('=============================================')
-            sys.stdout.flush()
+        #     batch_pred_label_ids = np.argmax(softmax_3d, axis=2)  # [batch_size, seq_length]
+        #     batch_gold_label_ids = y_b  # [batch_size, seq_length]
+        #     assert np.shape(batch_pred_label_ids) == (params.batch_size, np.shape(x1_b)[1])
+        #
+        #     # collect data for evaluation
+        #     for x1_row, x2_row, gold_label_ids, pred_label_ids, in zip(x1_b,
+        #                                                                x2_b,
+        #                                                                batch_gold_label_ids,
+        #                                                                batch_pred_label_ids):
+        #
+        #         sentence_length = len(x1_row) - count_zeros_from_end(x1_row)
+        #
+        #         assert count_zeros_from_end(x1_row) == count_zeros_from_end(gold_label_ids)
+        #         sentence_gold_labels = [data.sorted_labels[i] for i in gold_label_ids]
+        #         sentence_pred_labels = [data.sorted_labels[i] for i in pred_label_ids]
+        #         verb_index = np.argmax(x2_row)
+        #         sentence = [data.sorted_words[i] for i in x1_row]
+        #
+        #         # collect data for conll-05 evaluation + remove padding
+        #         all_sentence_pred_labels_no_pad.append(sentence_pred_labels[:sentence_length])
+        #         all_sentence_gold_labels_no_pad.append(sentence_gold_labels[:sentence_length])
+        #         all_verb_indices.append(verb_index)
+        #         all_sentences_no_pad.append(sentence[:sentence_length])
+        #
+        #         # collect data for per-label evaluation
+        #         all_gold_label_ids_no_pad += list(gold_label_ids[:sentence_length])
+        #         all_pred_label_ids_no_pad += list(pred_label_ids[:sentence_length])
+        #
+        # print('Number of sentences to evaluate: {}'.format(len(all_sentences_no_pad)))
+        #
+        # for label in all_sentence_gold_labels_no_pad:
+        #     assert label != config.Data.pad_label
+        #
+        # # evaluate f1 score computed over single labels (not spans)
+        # # f1_score expects 1D label ids (e.g. gold=[0, 2, 1, 0], pred=[0, 1, 1, 0])
+        # print_f1(epoch, 'weight', f1_score(all_gold_label_ids_no_pad, all_pred_label_ids_no_pad, average='weighted'))
+        # print_f1(epoch, 'macro ', f1_score(all_gold_label_ids_no_pad, all_pred_label_ids_no_pad, average='macro'))
+        # print_f1(epoch, 'micro ', f1_score(all_gold_label_ids_no_pad, all_pred_label_ids_no_pad, average='micro'))
+        #
+        # # evaluate with official conll05 perl script with Python interface provided by Allen AI NLP toolkit
+        # sys.stdout.flush()
+        # print('=============================================')
+        # print('Official Conll-05 Evaluation on Dev Split')
+        # dev_f1 = f1_official_conll05(all_sentence_pred_labels_no_pad,  # List[List[str]]
+        #                              all_sentence_gold_labels_no_pad,  # List[List[str]]
+        #                              all_verb_indices,  # List[Optional[int]]
+        #                              all_sentences_no_pad)  # List[List[str]]
+        # print_f1(epoch, 'conll-05', dev_f1)
+        # dev_f1s.append(dev_f1)
+        # print('=============================================')
+        # sys.stdout.flush()
 
         # ----------------------------------------------- end evaluation
 
@@ -216,36 +214,21 @@ def main(param2val):
             batch['verb_indicator'] = batch['verb_indicator'].cuda()
             batch['tags'] = batch['tags'].cuda()
 
-            a = batch['tokens']['tokens']
-            b = batch['verb_indicator']
-            c = batch['tags']
-
-            print(a)
-            print(b)
-            print(c)
-
-            assert a.is_cuda
-            assert b.is_cuda
-            assert c.is_cuda
-
-
+            # forward + loss
             optimizer.zero_grad()
-
             output_dict = model(**batch)  # input is dict[str, torch tensor]
             loss = output_dict["loss"] + model.get_regularization_penalty()
             if torch.isnan(loss):
                 raise ValueError("nan loss encountered")
 
+            # backward + update
             loss.backward()
             training_util.rescale_gradients(model, params.max_grad_norm)
             optimizer.step()
 
-            print(step, loss.item())  # TODO debugging
-
             if step % config.Eval.loss_interval == 0:
-                print('step {:<6}: loss={:2.2f} total minutes elapsed={:<3}'.format(
-                    step, loss.item(), (time.time() - train_start) // 60))
-
+                print('global-step={:<6}: loss={:2.2f} global-elapsed={:<3}minutes'.format(
+                    step * (epoch + 1), loss.item(), int((time.time() - train_start) // 60)))
 
     # to pandas
     eval_epochs = np.arange(params.max_epochs)
